882860ce46507b086295ae4acca29e4011599096
Vu Le <thenewvu@gmail.com>
2014-08-08 22:31:17 +0700

    * refactor

--- TodoReview.py
+++ TodoReview.py
@@ -5,31 +5,30 @@ A SublimeText 3 plugin for reviewing todo (any other) comments within your code.
 @author Jonathan Delgado (Initial Repo by @robcowie and ST3 update by @dnatag)
 '''
 
-from collections import namedtuple
-from datetime import datetime
-from itertools import groupby
 from os import path, walk
 import sublime_plugin
-import threading
 import sublime
+import threading
 import functools
 import fnmatch
 import re
 import sys
 import timeit
 
-Result = namedtuple('Result', 'match_name, match_text')
-
-
-def do_when(conditional, callback, *args, **kwargs):
-	if conditional():
-		return callback(*args, **kwargs)
-	sublime.set_timeout(functools.partial(do_when, conditional, callback, *args, **kwargs), 50)
-
 class Util():
 	@staticmethod
 	def log(message):
 		print("ReviewMyself: {0}".format(message))
+
+	@staticmethod
+	def status(message):
+		sublime.status_message("ReviewMyself: {0}".format(message))
+
+	@staticmethod
+	def doWhenSomethingDone(condition, callback, *args, **kwargs):
+		if condition():
+			return callback(*args, **kwargs)
+		sublime.set_timeout(functools.partial(Util.doWhenSomethingDone, condition, callback, *args, **kwargs), 50)
 
 class Settings():
 	def __init__(self, view):
@@ -59,11 +58,10 @@ class TodoSearchEngine(object):
 		for filepath in self.walk():
 			try:
 				file_stream = open(filepath, 'r', encoding='utf-8')
-				for linenum, line in enumerate(file_stream):
-					for mo in self.todo_filter.finditer(line):
-						matches = [Result(match_name, match_text) for match_name, match_text in mo.groupdict().items() if match_text]
-						for match in matches:
-							priority = self.priority_filter.search(match.match_text)
+				for linenum, line in enumerate(file_stream, 1):
+					for matches in self.todo_filter.finditer(line):
+						for match_name, match_text in matches.groupdict().items():
+							priority = self.priority_filter.search(match_text)
 
 							if priority:
 								priority = int(priority.group(0).replace('(', '').replace(')', ''))
@@ -72,12 +70,13 @@ class TodoSearchEngine(object):
 
 							yield {
 								'filepath': filepath,
-								'linenum': linenum + 1,
-								'match': match,
+								'linenum': linenum,
+								'todo': match_text,
 								'priority': priority
 							}
 			except:
-				Util.log("Read error, file: {0}, reason: {1}".format(filepath, sys.exc_info()))
+				# Util.log("Read error, file: {0}, reason: {1}".format(filepath, sys.exc_info()))
+				Util.log("Can not read {0}".format(filepath))
 				file_stream = None
 			finally:
 				self.counter.increment()
@@ -107,9 +106,9 @@ class ResultView():
 
 class TodoReviewShowResultCommand(sublime_plugin.TextCommand):
 	def run(self, edit, **args):
-
 		paths_to_search = args.get("paths_to_search", [])
 		results = args.get("results", [])
+		results = sorted(results, key=lambda result: (result['priority'])) # sort by proority
 		processed_file_count = args.get("processed_file_count", 0)
 		processed_time = args.get("processed_time", 0)
 
@@ -128,33 +127,28 @@ class TodoReviewShowResultCommand(sublime_plugin.TextCommand):
 		search_session_info += hr
 
 		result_view.insert(edit, result_view.size(), search_session_info)
+		
+		result_regions = []
 
-		regions_data = [x[:] for x in [[]] * 2]
-
-		results = sorted(results, key=lambda result: (result['priority']))
 		for index, result in enumerate(results, 1):
-			insert_point = result_view.size()
-
 			formatted_result = u'{index}. {filepath}:{linenum} => {todo}'.format(
 				index = index,
 				filepath = result["filepath"],
 				linenum = result['linenum'],
-				todo = result["match"][1])
+				todo = result["todo"])
 
-			result_view.insert(edit, insert_point, formatted_result)
-
-			region = sublime.Region(insert_point, result_view.size())
-			regions_data[0].append(region)
-			regions_data[1].append(result)
+			result_region_start = result_view.size()
+			result_view.insert(edit, result_region_start, formatted_result)
+			result_region_stop = result_view.size()
 			result_view.insert(edit, result_view.size(), u'\n')
 
-		result_view.add_regions('results', regions_data[0], '')
+			result_regions.append(sublime.Region(result_region_start, result_region_stop))
 
-		d_ = dict(('{0},{1}'.format(k.a, k.b), v) for k, v in zip(regions_data[0], regions_data[1]))
-		result_view.settings().set('result_regions', d_)
+		result_view.add_regions('result_regions', result_regions, '')
 
+		region_to_result_dict = dict(('{0},{1}'.format(region.a, region.b), result) for region, result in zip(result_regions, results));
+		result_view.settings().set('region_to_result_dict', region_to_result_dict)
 		sublime.active_window().focus_view(result_view)
-
 
 class SearchThread(threading.Thread):
 	def __init__(self, search_engine, onSearchingDone):
@@ -244,82 +238,71 @@ class TodoReviewCommand(sublime_plugin.TextCommand):
 	def render_formatted(self, rendered, counter):
 		self.window.run_command('render_result_run', {'formatted_results': rendered, 'file_counter': str(counter)})
 
-class NavigateResults(sublime_plugin.TextCommand):
-	def __init__(self, view):
-		super(NavigateResults, self).__init__(view)
-
+class TodoReviewNavigateResultCommand(sublime_plugin.TextCommand):
 	def run(self, edit, direction):
 		view_settings = self.view.settings()
-		results = self.view.get_regions('results')
+		result_regions = self.view.get_regions("result_regions")
+		result_region_cout = len(result_regions)
 
-		start_arr = {
-			'forward': -1,
-			'backward': 0,
-			'forward_skip': -1,
-			'backward_skip': 0
-		}
-
-		dir_arr = {
-			'forward': 1,
-			'backward': -1,
-			'forward_skip': settings.get('navigation_forward_skip', 10),
-			'backward_skip': settings.get('navigation_backward_skip', 10) * -1
-		}
-
-		if not results:
-			sublime.status_message('No results to navigate')
+		if result_region_cout <= 0:
 			return
 
-		selection = int(view_settings.get('selected_result', start_arr[direction]))
-		selection = selection + dir_arr[direction]
+		selected_index = int(view_settings.get("selected_index", -1))
+		
+		if direction == "up":
+			selected_index -= 1
+		elif direction == "down":
+			selected_index += 1
+		else:
+			Util.status("Incorrect navigation direction. Check settings!")
+			return
 
-		try:
-			target = results[selection]
-		except IndexError:
-			if selection < 0:
-				target = results[0]
-				selection = 0
-			else:
-				target = results[len(results) - 1]
-				selection = len(results) - 1
+		if selected_index < 0:
+			selected_index = result_region_cout - 1
+		elif selected_index > result_region_cout - 1:
+			selected_index = 0
+		
+		view_settings.set('selected_index', selected_index)
+		
+		selected_region = result_regions[selected_index]
+		self.view.add_regions('selected_region', [selected_region], 'selected', 'dot')
+		self.view.show(selected_region)
 
-		view_settings.set('selected_result', selection)
-		target = target.cover(target)
-		self.view.add_regions('selection', [target], 'selected', 'dot')
-		target.b = target.a + 5
-		self.view.show(target)
-
-class ClearSelection(sublime_plugin.TextCommand):
+class TodoReviewGotoCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		self.view.erase_regions('selection')
-		self.view.settings().erase('selected_result')
+		view_settings = self.view.settings()
+		result_regions = self.view.get_regions("result_regions")
+		result_region_cout = len(result_regions)
 
-class GotoComment(sublime_plugin.TextCommand):
-	def __init__(self, *args):
-		super(GotoComment, self).__init__(*args)
+		if result_region_cout <= 0:
+			return
 
-	def run(self, edit):
-		selection = int(self.view.settings().get('selected_result', -1))
-		selected_region = self.view.get_regions('results')[selection]
+		selected_index = int(view_settings.get("selected_index", -1))
+		if selected_index < 0 or selected_index > result_region_cout - 1:
+			Util.status("Select a todo first!")
+			return;
 
-		data = self.view.settings().get('result_regions')['{0},{1}'.format(selected_region.a, selected_region.b)]
-		new_view = self.view.window().open_file(data['filepath'])
-		do_when(lambda: not new_view.is_loading(), lambda:new_view.run_command('goto_line', {'line': data['linenum']}))
+		selected_region = result_regions[selected_index]
+		region_to_result_dict = self.view.settings().get('region_to_result_dict')
 
-class MouseGotoComment(sublime_plugin.TextCommand):
-    def __init__(self, *args):
-        super(MouseGotoComment, self).__init__(*args)
+		result = region_to_result_dict['{0},{1}'.format(selected_region.a, selected_region.b)]
+		new_view = self.view.window().open_file(result['filepath'])
+		Util.doWhenSomethingDone(lambda: not new_view.is_loading(), lambda:new_view.run_command('goto_line', {'line': result['linenum']}))
 
-    def run(self, edit):
-        if not self.view.settings().get('result_regions'):
-            return
+# Reference: https://github.com/bradrobertson/sublime-packages/blob/master/Default/goto_line.py
+class GotoLineCommand(sublime_plugin.TextCommand):
+	def run(self, edit, line):
+		# Convert from 1 based to a 0 based line number
+		line = int(line) - 1
 
-        result = self.view.line(self.view.sel()[0].end())
+		# Negative line numbers count from the end of the buffer
+		if line < 0:
+			lines, _ = self.view.rowcol(self.view.size())
+			line = lines + line + 1
 
-        target = result.cover(result)
-        self.view.add_regions('selection', [target], 'selected', 'dot')
-        self.view.show(target)
+		pt = self.view.text_point(line, 0)
 
-        data = self.view.settings().get('result_regions')['{0},{1}'.format(result.a, result.b)]
-        new_view = self.view.window().open_file(data['filepath'])
-        do_when(lambda: not new_view.is_loading(), lambda: new_view.run_command("goto_line", {"line": data['linenum']}))
+		self.view.sel().clear()
+		self.view.sel().add(sublime.Region(pt))
+
+		self.view.show_at_center(pt)
