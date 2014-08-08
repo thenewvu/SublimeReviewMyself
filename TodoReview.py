9ecbcc600e61ac6007ce3a4645b0b3a1c325eba2
Vu Le <thenewvu@gmail.com>
2014-08-08 17:28:25 +0700

    * refactor

--- TodoReview.py
+++ TodoReview.py
@@ -16,6 +16,7 @@ import functools
 import fnmatch
 import re
 import sys
+import timeit
 
 Result = namedtuple('Result', 'match_name, match_text')
 
@@ -43,7 +44,7 @@ class TodoSearchEngine(object):
 		self.paths_to_search = []
 		self.todo_filter = None
 		self.priority_filter = None
-		self.counter = None
+		self.counter = Counter()
 
 	def walk(self):
 		for path_to_search in self.paths_to_search:
@@ -83,34 +84,68 @@ class TodoSearchEngine(object):
 				if file_stream is not None:
 					file_stream.close()
 
-class RenderResultRunCommand(sublime_plugin.TextCommand):
-	def run(self, edit, formatted_results, file_counter):
+class ResultView():
+	@staticmethod
+	def get():
 		active_window = sublime.active_window()
-		existing_results = [v for v in active_window.views() if v.name() == 'TodoReview' and v.is_scratch()]
-		if existing_results:
-			result_view = existing_results[0]
+		#TODO: change result view attributes
+		existed_result_view = [view for view in active_window.views() if view.name() == 'TodoReview' and view.is_scratch()]
+		if existed_result_view:
+			result_view = existed_result_view[0]
 		else:
 			result_view = active_window.new_file()
 			result_view.set_name('TodoReview')
 			result_view.set_scratch(True)
+			result_view.settings().set('line_padding_bottom', 2)
+			result_view.settings().set('line_padding_top', 2)
+			result_view.settings().set('word_wrap', False)
+			result_view.settings().set('command_mode', True)
 			result_view.settings().set('todo_results', True)
+			result_view.assign_syntax('Packages/TodoReview/TodoReview.hidden-tmLanguage')
 
-		hr = u'+ {0} +'.format('-' * 56)
-		header = u'{hr}\n| TodoReview @ {0:<43} |\n| {1:<56} |\n{hr}\n'.format(datetime.now().strftime('%A %m/%d/%y at %I:%M%p'), u'{0} files scanned'.format(file_counter), hr=hr)
+		return result_view
 
+class TodoReviewShowResultCommand(sublime_plugin.TextCommand):
+	def run(self, edit, **args):
+
+		paths_to_search = args.get("paths_to_search", [])
+		results = args.get("results", [])
+		processed_file_count = args.get("processed_file_count", 0)
+		processed_time = args.get("processed_time", 0)
+
+		result_view = ResultView.get()
 		result_view.erase(edit, sublime.Region(0, result_view.size()))
-		result_view.insert(edit, result_view.size(), header)
+
+		hr = "-" * 100 + "\n"
+		search_session_info = ""
+
+		search_session_info += hr
+		search_session_info += "Searched in:\n"
+		for path_to_search in paths_to_search:
+			search_session_info += "\t{0}\n".format(path_to_search)
+		search_session_info += "Processed file count: {0}\n".format(processed_file_count)
+		search_session_info += "Processed time: {0}s\n".format(processed_time)
+		search_session_info += hr
+
+		result_view.insert(edit, result_view.size(), search_session_info)
 
 		regions_data = [x[:] for x in [[]] * 2]
 
-
-		for linetype, line, data in formatted_results:
+		results = sorted(results, key=lambda result: (result['priority']))
+		for index, result in enumerate(results, 1):
 			insert_point = result_view.size()
-			result_view.insert(edit, insert_point, line)
-			if linetype == 'result':
-				rgn = sublime.Region(insert_point, result_view.size())
-				regions_data[0].append(rgn)
-				regions_data[1].append(data)
+
+			formatted_result = u'{index}. {filepath}:{linenum} => {todo}'.format(
+				index = index,
+				filepath = result["filepath"],
+				linenum = result['linenum'],
+				todo = result["match"][1])
+
+			result_view.insert(edit, insert_point, formatted_result)
+
+			region = sublime.Region(insert_point, result_view.size())
+			regions_data[0].append(region)
+			regions_data[1].append(result)
 			result_view.insert(edit, result_view.size(), u'\n')
 
 		result_view.add_regions('results', regions_data[0], '')
@@ -118,43 +153,39 @@ class RenderResultRunCommand(sublime_plugin.TextCommand):
 		d_ = dict(('{0},{1}'.format(k.a, k.b), v) for k, v in zip(regions_data[0], regions_data[1]))
 		result_view.settings().set('result_regions', d_)
 
-		result_view.assign_syntax('Packages/TodoReview/TodoReview.hidden-tmLanguage')
-		result_view.settings().set('line_padding_bottom', 2)
-		result_view.settings().set('line_padding_top', 2)
-		result_view.settings().set('word_wrap', False)
-		result_view.settings().set('command_mode', True)
-		active_window.focus_view(result_view)
+		sublime.active_window().focus_view(result_view)
+
 
 class SearchThread(threading.Thread):
-	def __init__(self, search_engine, callback, counter):
+	def __init__(self, search_engine, onSearchingDone):
 		self.search_engine = search_engine
-		self.callback = callback
-		self.counter = counter
+		self.onSearchingDone = onSearchingDone
 		threading.Thread.__init__(self)
 
 	def run(self):
-		results = self.search_engine.search()
-		formatted_results = list(self.format(results))
-		self.callback(formatted_results, self.counter)
-
-	def format(self, results):
-		results = sorted(results, key=lambda result: (result['priority']))
-		for index, result in enumerate(results, 1):
-			line = u'{index}. {filepath}:{linenum}: {match_text}'.format(
-				index=index,
-				filepath=result["filepath"],
-				linenum=result['linenum'],
-				match_text=result["match"].match_text)
-
-			yield ('result', line, result)
+		self.search_engine.counter.startTimer()
+		results = list(self.search_engine.search())
+		self.search_engine.counter.stopTimer()
+		self.onSearchingDone(results, self.search_engine.counter)
 					
 class Counter(object):
 	def __init__(self):
 		self.current = 0
+		self.start_time = 0
+		self.stop_time = 0
 		self.lock = threading.RLock()
 
 	def __str__(self):
 		return "{0}".format(self.current)
+
+	def startTimer(self):
+		self.start_time = timeit.default_timer()
+
+	def stopTimer(self):
+		self.stop_time = timeit.default_timer()
+
+	def getDeltaTime(self):
+		return self.stop_time - self.start_time
 
 	def increment(self):
 		with self.lock:
@@ -166,28 +197,28 @@ class TodoReviewImpl(sublime_plugin.TextCommand):
 		global settings
 		settings = Settings(self.view)
 
-		if "paths_to_search" in args:
-			self.paths_to_search = args["paths_to_search"]
-		else:
-			self.paths_to_search = []
+		self.paths_to_search = args.get("paths_to_search", [])
 		self.is_ignore_case = settings.get("is_ignore_case", True)
-		self.todo_patterns = settings.get("todo_patterns", {})
-		self.priority_patterns = settings.get("priority_patterns", {})
-		self.counter = Counter()
+		self.todo_patterns = settings.get("todo_patterns", [])
+		self.priority_patterns = settings.get("priority_patterns", [])
 
 		Util.log("paths_to_search = {0}".format(self.paths_to_search))
 
-		search_engine = TodoSearchEngine()
-		search_engine.paths_to_search = self.paths_to_search
-		search_engine.counter = self.counter
-		search_engine.todo_filter = re.compile("|".join(self.todo_patterns), re.IGNORECASE if self.is_ignore_case else 0)
-		search_engine.priority_filter = re.compile("|".join(self.priority_patterns), re.IGNORECASE if self.is_ignore_case else 0)
+		self.search_engine = TodoSearchEngine()
+		self.search_engine.paths_to_search = self.paths_to_search
+		self.search_engine.todo_filter = re.compile("|".join(self.todo_patterns), re.IGNORECASE if self.is_ignore_case else 0)
+		self.search_engine.priority_filter = re.compile("|".join(self.priority_patterns), re.IGNORECASE if self.is_ignore_case else 0)
 
-		self.search_thread = SearchThread(search_engine, self.onSearchingDone, self.counter)
+		self.search_thread = SearchThread(self.search_engine, self.onSearchingDone)
 		self.search_thread.start()
 
-	def onSearchingDone(self, rendered, counter):
-		self.view.run_command('render_result_run', {'formatted_results': rendered, 'file_counter': str(self.counter)})
+	def onSearchingDone(self, results, counter):
+		self.view.run_command("todo_review_show_result", {
+			"paths_to_search": self.paths_to_search,
+			"results": results,
+			"processed_file_count": counter.current,
+			"processed_time": counter.getDeltaTime()
+			})
 
 class TodoReviewAutoModeCommand(sublime_plugin.TextCommand):
 	def run(self, edit, **args):
